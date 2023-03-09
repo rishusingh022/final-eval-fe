007bbd1fce28535d8d60e72dcd1e2d213b322f91
rishusingh022 <rishusingh022@gmail.com>
2023-03-09 18:33:06 +0530

    feat : add error pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ErrorPage, HomePage, NotFoundPage } from './pages';
+import { ERROR_ROUTE, HOME_ROUTE, NOT_FOUND_ROUTE } from './constant/routes';
 function App() {
   return (
     <div className="App">
-      App
+      <BrowserRouter>
+        <Routes>
+          <Route path={HOME_ROUTE} element={<HomePage />} />
+          <Route path={ERROR_ROUTE} element={<ErrorPage />} />
+          <Route path={NOT_FOUND_ROUTE} element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
