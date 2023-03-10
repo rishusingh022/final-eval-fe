235df816ed9a5e93b0a078c56b4093a7365d7079
rishusingh022 <rishusingh022@gmail.com>
2023-03-10 14:20:18 +0530

    opt : side modal

--- src/components/SideModal/index.jsx
+++ src/components/SideModal/index.jsx
@@ -5,21 +5,13 @@ import { useNavigate } from 'react-router-dom';
 
 function SideModal() {
   const navigate = useNavigate();
-  const [fieldName, setFieldName] = React.useState('');
-  const [fieldType, setFieldType] = React.useState('');
+
   const [modal, setModal] = React.useState(true);
 
   const toggleModal = () => {
     setModal(!modal);
   };
 
-  const handleFieldNameChange = (e) => {
-    setFieldName(e.target.value);
-  };
-
-  const handleFieldTypeChange = (e) => {
-    setFieldType(e.target.value);
-  };
   return (
     <>
       {modal && (
@@ -31,77 +23,23 @@ function SideModal() {
             </div>
             <div className="modal-body">
               <label htmlFor="name">FieldName</label>
-              <input
-                onChange={handleFieldNameChange}
-                value={fieldName}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
               <label htmlFor="name">Type of field</label>
-              <input
-                onChange={handleFieldTypeChange}
-                value={fieldType}
-                type="formName"
-                name="formName"
-                id="formName"
-              />
+              <input type="formName" name="formName" id="formName" />
             </div>
             <div className="modal-footer">
               <button className="cancel" onClick={toggleModal}>
